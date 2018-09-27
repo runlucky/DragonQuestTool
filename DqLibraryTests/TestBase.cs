f8378d0850693b312cd93fe484a5400b4b93f12f
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-27 23:00:14 +0900

    中身ちょっとつくる

---
+++ DqLibraryTests/TestBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DqLibraryTests
+{
+    [TestClass]
+    public abstract class TestBase
+    {
+        public TestContext TestContext { get; set; }
+    }
+}
