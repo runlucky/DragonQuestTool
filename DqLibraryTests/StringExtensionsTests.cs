3dde87ff84d649199e985d90df3ef59f899b6c6a
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-27 22:42:40 +0900

    テストプロジェクトの側作成

---
+++ DqLibraryTests/StringExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DqLibraryTests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void TestToInt()
+        {
+        }
+    }
+}
---
+++ DqToolTests/Class/CalcTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DqTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DqTool.Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]
+        public void IsHMTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void IsHTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetNumTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void IsMatchTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ScanTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
